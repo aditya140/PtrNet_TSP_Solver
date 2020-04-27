f1ed4953d0fd350d5ce0d5b29a5a39c28033f038
sameeradh <adh2sam@gmail.com>
2020-04-27 14:23:11 -0400

    updated google or tools to automatic

--- tsp_solver/googleORsolver.py
+++ tsp_solver/googleORsolver.py
@@ -83,7 +83,7 @@ def GoogleORsolver(dist_mat,isFloat=True):
         # Setting first solution heuristic.
         search_parameters = pywrapcp.DefaultRoutingSearchParameters()
         search_parameters.first_solution_strategy = (
-            routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+            routing_enums_pb2.FirstSolutionStrategy.AUTOMATIC)
 
         # Solve the problem.
         solution = routing.SolveWithParameters(search_parameters)
